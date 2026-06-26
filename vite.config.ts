bf3df240b94615a7786d73d05ab90ee3acffdc3d
waykegoat <raggyprod@gmail.com>
2026-06-26 22:59:11 +0300

    add github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/kitchenroom/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
